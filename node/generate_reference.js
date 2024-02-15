e61edecf0cbe85068a0b6e41b58594cd4b1d1bef
Noah <66693138+n0j0games@users.noreply.github.com>
2024-02-15 15:06:38 +0100

    added refrence crawling

--- node/generate_reference.js
+++ node/generate_reference.js
@@ -1,32 +1,34 @@
 const fs = require('fs');
 const readline = require('readline');
+const Xray = require('x-ray');
+const x = Xray();
 
 let json = {
     "references": []
 };
 
-async function processLineByLine() {
-  const fileStream = fs.createReadStream('reference.txt');
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-  for await (const line_ of rl) {
-    let split = line_.split('<');
-    const href = "https://minecraft.wiki" + line_.split('href="')[2].split('"')[0];
-    let src = "https://minecraft.wiki" + split[3].split('src="')[1].split('"')[0].split("?")[0].split("/30px")[0];
-    src = src.replace("/thumb", "");
-    const title = line_.split('title="')[1].split('"')[0].toLowerCase().trim();
-    
-    json.references.push({"block" : title, "image" : src, "href" : href});
-
-    // <li><a href="/w/File:Zombie_Head_(8).png" class="image"><img alt="Zombie Head (8).png" src="/images/thumb/Zombie_Head_%288%29.png/30px-Zombie_Head_%288%29.png?af5c1" decoding="async" loading="lazy" width="30" height="30" srcset="/images/thumb/Zombie_Head_%288%29.png/60px-Zombie_Head_%288%29.png?af5c1 2x" data-file-width="300" data-file-height="300"></a> <a href="/w/Zombie_Head" title="Zombie Head">Zombie Head</a></li>
-  }
-  fs.writeFile(`../references.json`, JSON.stringify(json), function(err) {
-    if (err) { console.log(err); }
-    else { console.log("File saved."); }
+function gen() {
+  x('https://minecraft.wiki/w/Block#List_of_blocks', '.div-col ul li', [{
+    title: ['a@title'],
+    src: 'img@src',
+    href: ['a@href']
+  }])(function(err, obj) {
+      if (err) { console.log(err); }
+      else {
+          obj.forEach((item) => {
+              console.log(item);
+              item.src = item.src.split("?")[0].split("/30px")[0];
+              item.src = item.src.replace("/thumb", "");
+              item.href = item.href[1];
+              item.title = item.title[item.title.length-1].toLowerCase().trim();
+              json.references.push({"block" : item.title, "image" : item.src, "href" : item.href});
+          });
+          fs.writeFile(`../references.json`, JSON.stringify(json), function(err) {
+            if (err) { console.log(err); }
+            else { console.log("File saved."); }
+          });
+      }
   });
 }
 
-processLineByLine();
+gen();
